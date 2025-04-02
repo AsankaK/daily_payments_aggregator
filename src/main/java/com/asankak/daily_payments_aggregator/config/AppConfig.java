2c6ee7b8f3ed198703d1cd0a983c9735e3850833
AsankaK <saradeasa@gmail.com>
2025-04-02 15:34:39 +0200

    fix: fixed import

--- src/main/java/com/asankak/daily_payments_aggregator/config/AppConfig.java
+++ src/main/java/com/asankak/daily_payments_aggregator/config/AppConfig.java
@@ -1,15 +1,12 @@
 package com.asankak.daily_payments_aggregator.config;
 
-import java.io.IOException;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Configuration;
-
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "app")
 @ConfigurationPropertiesScan
@@ -23,7 +20,7 @@ public class AppConfig {
 	String currencyRateFilePath;
 
 	@PostConstruct
-	void logInitValues() throws IOException {
+	void logInitValues(){
 		log.info("Payment file path {}", paymentFilePath);
 		log.info("Currency file path {}", currencyRateFilePath);
 	}
